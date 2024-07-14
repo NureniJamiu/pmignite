9beeaa0bee6348af7de9f5b4bc323e78c9597837
NureniJamiu <nurenijamiu951@gmail.com>
2024-07-14 12:54:12 +0100

    add footer

--- src/components/custom/Teams.tsx
+++ src/components/custom/Teams.tsx
@@ -86,33 +86,11 @@ const Card = ({
                     <Image src={image} alt="Team image" layout="fill"
                         objectFit="cover" />
                 </div>
-                <h2 className="dark:text-white text-xl opacity-0 group-hover/canvas-card:opacity-100 relative z-10 text-black mt-4  font-bold group-hover/canvas-card:text-white group-hover/canvas-card:-translate-y-2 transition duration-200">
+                <h2 className="dark:text-white text-xl opacity-0 group-hover/canvas-card:opacity-100 relative z-10 text-black mt-4 font-bold group-hover/canvas-card:text-white group-hover/canvas-card:-translate-y-2 transition duration-200">
                     {title}
                 </h2>
             </div>
         </div>
-    );
-};
-
-const AceternityIcon = () => {
-    return (
-        <svg
-            width="66"
-            height="65"
-            viewBox="0 0 66 65"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-10 w-10 text-black dark:text-white group-hover/canvas-card:text-white "
-        >
-            <path
-                d="M8 8.05571C8 8.05571 54.9009 18.1782 57.8687 30.062C60.8365 41.9458 9.05432 57.4696 9.05432 57.4696"
-                stroke="currentColor"
-                strokeWidth="15"
-                strokeMiterlimit="3.86874"
-                strokeLinecap="round"
-                style={{ mixBlendMode: "darken" }}
-            />
-        </svg>
     );
 };
 
