35df5d2e3637bbc1935b6880bca7ba9af325a3e1
NureniJamiu <nurenijamiu951@gmail.com>
2024-07-15 13:09:30 +0100

    responsive footer

--- src/components/custom/Footer.tsx
+++ src/components/custom/Footer.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Footer = () => {
     return <div className="bg-background">
         <div className="max-w-6xl mx-auto px-5 flex flex-col md:flex-row gap-3 md:gap-0 items-center justify-between py-4">
-            <div className="text-gray-300 text-center">© 2024 PMIgnite. All rights reserved</div>
+            <div className="text-gray-300 text-center text-sm md:text-base">© 2024 PMIgnite. All rights reserved</div>
             <div className="flex items-center justify-between gap-4">
                 <Mail size={20} />
                 <Twitter size={20} />
