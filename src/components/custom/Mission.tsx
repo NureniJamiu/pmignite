5792c301b9d0ec0b9df96c56d89e5f745ace0d01
NureniJamiu <nurenijamiu951@gmail.com>
2024-07-15 12:58:14 +0100

    responsive mission

--- src/components/custom/Mission.tsx
+++ src/components/custom/Mission.tsx
@@ -6,17 +6,17 @@ import MISSION_IMAGE from "@/images/mission.svg";
 import Image from "next/image";
 
 const Mission = () => {
-    return <div className="relative w-full rounded-t bg-background p-5 md:p-20 md:shadow-xl md:pt-28 pb-48 overflow-hidden">
+    return <div className="relative w-full rounded-t bg-background p-5 pt-14 md:p-20 md:shadow-xl md:pt-28 pb-48 overflow-hidden">
         <div className="flex flex-col gap-8 md:gap-0 md:flex-row max-w-6xl mx-auto">
             <div className="md:flex-1">
-                <div className="flex items-center justify-center md:justify-normal gap-3">
-                    <div className="hidden md:flex items-center justify-center size-8 rounded-full p-1.5 bg-indigo-300">
+                <div className="flex items-center md:justify-normal gap-3">
+                    <div className="flex items-center justify-center size-8 rounded-full p-1.5 bg-indigo-300">
                         <TrendingUp className=" text-indigo-700" />
                     </div>
                     <span className="dark:text-slate-300 ">Our mission</span>
                 </div>
                 <div>
-                    <h1 className="text-center md:text-left dark:text-slate-300 text-3xl font-semibold md:font-medium md:text-5xl my-3">What drives us</h1>
+                    <h1 className="dark:text-slate-300 text-4xl md:font-medium md:text-5xl my-3">What drives us</h1>
                     <p className="dark:text-slate-400 max-w-xl md:text-lg pt-3 md:pt-0">Our mission is to bridge the gap between emerging project management talent and
                         growth-oriented start-ups/companies. By connecting inexperienced project managers
                         with organizations in need of strategic guidance, we aim to foster mutual growth,
