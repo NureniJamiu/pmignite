6e3f54aaa86eebb5ade762e46812c8e53846f8d0
NureniJamiu <nurenijamiu951@gmail.com>
2024-07-19 15:38:48 +0100

    Change card icons

--- src/components/custom/CompaniesBenefits.tsx
+++ src/components/custom/CompaniesBenefits.tsx
@@ -1,27 +1,27 @@
 import React from "react";
 import { BentoGrid, BentoGridItem } from "../ui/bento-grid";
-import { Clipboard, Columns, File, Signal, Waves } from "lucide-react";
+import { Activity, Bot, Goal, PieChart, Waves } from "lucide-react";
 
 const items = [
     {
         title: "Expertise at no cost",
         description: "Access to skilled project managers who volunteer their expertise to help streamline your projects and improve operational efficiency.",
         icon: <div className="flex items-center justify-center size-8 rounded-full bg-blue-400">
-            <Clipboard className="text-blue-800 h-4 w-4" />
+            <Activity className="text-blue-800 h-4 w-4" />
         </div>,
     },
     {
         title: "Customized support",
         description: "Tailored project management solutions to meet your specific business needs, ensuring optimal results.",
         icon: <div className="flex items-center justify-center size-8 rounded-full bg-green-400">
-            <File className="text-green-800 h-4 w-4" />
+            <Bot className="text-green-800 h-4 w-4" />
         </div>,
     },
     {
         title: "Increased productivity",
         description: "Our project managers focus on enhancing your workflow, leading to better time management and increased productivity.",
         icon: <div className="flex items-center justify-center size-8 rounded-full bg-pink-400">
-            <Signal className="text-pink-800 h-4 w-4" />
+            <PieChart className="text-pink-800 h-4 w-4" />
         </div>,
     },
     {
@@ -29,7 +29,7 @@ const items = [
         description:
             "With dedicated support, you can focus on your core business activities while we help you achieve your project goals.",
         icon: <div className="flex items-center justify-center size-8 rounded-full bg-purple-400">
-            <Columns className="text-purple-800 h-4 w-4" />
+            <Goal className="text-purple-800 h-4 w-4" />
         </div>,
     },
     {
--- src/components/custom/PmBenefits.tsx
+++ src/components/custom/PmBenefits.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BentoGrid, BentoGridItem } from "../ui/bento-grid";
-import { Clipboard, Columns, File, Signal, Waves } from "lucide-react";
+import { Blocks, LayoutList, Network, Rocket } from "lucide-react";
 import LinearGradient from "../magicui/linear-gradient";
 
 const items = [
@@ -9,27 +9,27 @@ const items = [
         description: "Access to skilled project managers who volunteer their expertise to help streamline your projects and improve operational efficiency.",
         className: "md:col-span-2",
         icon: <div className="flex items-center justify-center size-8 rounded-full bg-blue-400">
-            <Clipboard className="text-blue-800 h-4 w-4" />
+            <LayoutList className="text-blue-800 h-4 w-4" />
         </div>,
     },
     {
         title: "Professional growth",
         description: "Tailored project management solutions to meet your specific business needs, ensuring optimal results.",
         className: "md:col-span-1",
-        icon: <div className="flex items-center justify-center size-8 rounded-full bg-green-400"><File className="h-4 w-4 text-green-800" /></div>,
+        icon: <div className="flex items-center justify-center size-8 rounded-full bg-green-400"><Rocket className="h-4 w-4 text-green-800" /></div>,
     },
     {
         title: "Networking opportunities",
         description: "Our project managers focus on enhancing your workflow, leading to better time management and increased productivity.",
         className: "md:col-span-1",
-        icon: <div className="flex items-center justify-center size-8 rounded-full bg-pink-400"><Signal className="h-4 w-4 text-pink-800" /></div>,
+        icon: <div className="flex items-center justify-center size-8 rounded-full bg-pink-400"><Network className="h-4 w-4 text-pink-800" /></div>,
     },
     {
         title: "Skill development",
         description:
             "With dedicated support, you can focus on your core business activities while we help you achieve your project goals.",
         className: "md:col-span-2",
-        icon: <div className="flex items-center justify-center size-8 rounded-full bg-purple-400"><Columns className="h-4 w-4 text-purple-800" /></div>,
+        icon: <div className="flex items-center justify-center size-8 rounded-full bg-purple-400"><Blocks className="h-4 w-4 text-purple-800" /></div>,
     },
 ];
 
